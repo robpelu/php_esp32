dd7130df4541fd17e4521e6f19563a98c946ded4
Robert Pérez <33105729+robpelu@users.noreply.github.com>
2020-02-25 16:19:29 -0300

    Update enviar_cmd.php

--- enviar_cmd.php
+++ enviar_cmd.php
@@ -6,8 +6,8 @@ $database = "d79gdtjvldehc6";
 $username = "tsmjrcqunicnlx";
 $password = "156c2dadd79b76cf6782243d0c90c1af4f3003ae32a1d0c12157f815517db330";
 
-$espid = $_POST["espid"]; //igual a nombre en consulta "select"
-$cmd = $_POST["cmd"];
+$espid = $_GET["espid"]; //igual a nombre en consulta "select"
+$cmd = $_GET["cmd"];
 
 // Conectando y seleccionado la base de datos  
 $dbconn = pg_connect("host=" . $servername . " dbname=" . $database . " user=". $username . " password=" . $password)
